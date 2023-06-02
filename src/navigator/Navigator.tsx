ea781d1e2843252ebce9546f3d457f98dce2ee0c
Andres Melo <andresfelipemelo18@gmail.com>
2023-06-02 16:04:45 -0500

    first commit

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from '../pages/MapScreen';
+import PermissionsScreen from '../pages/PermissionsScreen';
+import LoadingScreen from '../pages/LoadingScreen';
+
+const Stack = createStackNavigator();
+
+export const  Navigator=()=> {
+  return (
+    <Stack.Navigator 
+    initialRouteName='PermissionsScreen'
+    screenOptions={{
+      
+      headerShown:false,
+      cardStyle:{
+        backgroundColor:'white'
+      }
+    }}>
+      <Stack.Screen name="MapScreen" component={MapScreen} />
+      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+
+    </Stack.Navigator>
+  );
+}
