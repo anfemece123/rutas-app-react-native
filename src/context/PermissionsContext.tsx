683ac69ed72313738fc5d5b2b3ebafe8ed1d6ad7
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2023-06-20 22:26:54 -0500

    commit

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,93 +1,83 @@
-import React,{ createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { AppState, Platform } from "react-native";
-import { PERMISSIONS, PermissionStatus, check, request } from "react-native-permissions";
+import {
+  PERMISSIONS,
+  PermissionStatus,
+  check,
+  request,
+} from "react-native-permissions";
 
-
-export interface PermissionsState{ 
-    locationStatus: PermissionStatus;
+export interface PermissionsState {
+  locationStatus: PermissionStatus;
 }
 
-export const permissionInitState: PermissionsState={
-    locationStatus:'unavailable',
-}
+export const permissionInitState: PermissionsState = {
+  locationStatus: "unavailable",
+};
 
-type PermissionsContextProps={
-    permissions: PermissionsState;
-    askLocationPermission: () =>void;
-    checkLocationPermission:()=>void;
-}
+type PermissionsContextProps = {
+  permissions: PermissionsState;
+  askLocationPermission: () => void;
+  checkLocationPermission: () => void;
+};
 
+export const PermissionsContext = createContext({} as PermissionsContextProps); //TODO: QUE EXPORTA
 
-export const PermissionsContext=  createContext({} as PermissionsContextProps) ; //TODO: QUE EXPORTA 
+export const PermissionsProvider = ({ children }: any) => {
+  const [permissions, setPermissions] = useState(permissionInitState);
 
-export const PermissionsProvider= ({children}: any)=>{
-    const [permissions, setPermissions] = useState(permissionInitState);
+  useEffect(() => {
+    AppState.addEventListener("change", (state) => {
+      console.log(state); //verifica en que estado esta la pantalla del dispositivo
+      if (state !== "active") return; //SI NO ESTA ACTIVE SE SALE
 
-    useEffect(() => {
-     AppState.addEventListener('change',state=>{
-        console.log(state);//verifica en que estado esta la pantalla del dispositivo 
-        if(state!=='active')return;
+      checkLocationPermission(); //si esta active
+    });
+  }, []);
 
-        checkLocationPermission();//si esta active
-     })
+  const askLocationPermission = async () => {
+    let permissionStatus: PermissionStatus; //* VARIABLE SOLA
 
-
-    }, [])
-    
-
-    const askLocationPermission= async()=>{
-
-        let permissionStatus: PermissionStatus; //* VARIABLE SOLA 
-
-        if(Platform.OS==='ios'){
-
-        //    permissionStatus = await  check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-           permissionStatus = await  request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
-        }else{
-            // permissionStatus = await  check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-            permissionStatus = await  request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
-        }
-        setPermissions({
-            ...permissions,
-            locationStatus:permissionStatus
-        })
+    if (Platform.OS === "ios") {
+      //    permissionStatus = await  check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      // permissionStatus = await  check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION); /}
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+      ); //el request pregunta no solo REVISA COMO EL CHACK
     }
-    const checkLocationPermission=async()=>{
-        let permissionStatus: PermissionStatus; //* VARIABLE SOLA 
+    setPermissions({
+      ...permissions,
+      locationStatus: permissionStatus,
+    });
+  };
+  const checkLocationPermission = async () => {
+    // cuando la persona regresa a la aplicacion
+    let permissionStatus: PermissionStatus; //* VARIABLE SOLA
 
-        if(Platform.OS==='ios'){
-
-           permissionStatus = await  check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-        //    permissionStatus = await  request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
-        }else{
-            permissionStatus = await  check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-            // permissionStatus = await  request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
-        }
-        setPermissions({
-            ...permissions,
-            locationStatus:permissionStatus
-        })
-
+    if (Platform.OS === "ios") {
+      permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE); //el check revisa el estado NO PREGUNTA
+      //    permissionStatus = await  request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      // permissionStatus = await  request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
     }
+    setPermissions({
+      ...permissions,
+      locationStatus: permissionStatus,
+    });
+  };
 
-
-    return(
-        <PermissionsContext.Provider value={{
-            permissions,
-            askLocationPermission,
-            checkLocationPermission
-
-
-        }}>
-
-            {children}
-        </PermissionsContext.Provider>
-    )
-
-}
-
-
+  return (
+    <PermissionsContext.Provider
+      value={{
+        permissions,
+        askLocationPermission,
+        checkLocationPermission,
+      }}
+    >
+      {children}
+    </PermissionsContext.Provider>
+  );
+};
--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,27 +1,35 @@
-
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import MapScreen from '../pages/MapScreen';
-import PermissionsScreen from '../pages/PermissionsScreen';
-import LoadingScreen from '../pages/LoadingScreen';
+import React, { useContext } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import MapScreen from "../pages/MapScreen";
+import PermissionsScreen from "../pages/PermissionsScreen";
+import LoadingScreen from "../pages/LoadingScreen";
+import { PermissionsContext } from "../context/PermissionsContext";
 
 const Stack = createStackNavigator();
 
-export const  Navigator=()=> {
-  return (
-    <Stack.Navigator 
-    initialRouteName='PermissionsScreen'
-    screenOptions={{
-      
-      headerShown:false,
-      cardStyle:{
-        backgroundColor:'white'
-      }
-    }}>
-      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
-      <Stack.Screen name="MapScreen" component={MapScreen} />
-      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+export const Navigator = () => {
+  const { permissions } = useContext(PermissionsContext);
 
+  if (permissions.locationStatus === "unavailable") {
+    //COMO INICIALMENTE TENIAMOS UNAVAILABLE SE MUESTRA EL LOADING PERO DESPUES YA NOS MUESTRA LA PAGINA COMO ES
+    return <LoadingScreen />;
+  }
+  return (
+    <Stack.Navigator
+      initialRouteName="PermissionsScreen"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
+      {permissions.locationStatus === "granted" ? (
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      )}
+      {/* <Stack.Screen name="LoadingScreen" component={LoadingScreen} /> */}
     </Stack.Navigator>
   );
-}
+};
